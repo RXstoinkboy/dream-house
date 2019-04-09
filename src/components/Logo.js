0034a1a01cb50972ec7d5befe070010870597a37
RXstoinkboy <erykludwin@gmail.com>
2019-04-10 00:37:42 +0200

    find a way to test if clickning on Logo redirexts to '/'

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import styles from './styles/Logo.module.css'
+import logo from '../assets/img/logo.jpg'
 
 const Logo = () => {
     return (
         <div className={styles.wrapper}>
-          logo  
+          <Link to='/lala'>
+            <img src={logo} alt="dream house"/>  
+          </Link>
         </div>
     );
 };
